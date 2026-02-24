3f710e454d04b0158866e53ced4f68b8e937cc2c
Miguel Contreras <miguelacontreras08@gmail.com>
2026-02-24 10:05:30 -0700

    Added usage of cin to basics file

--- basics.cpp
+++ basics.cpp
@@ -21,10 +21,26 @@ int main(){
     std::cout << std::endl;
     printSum(15, 20);
     std::cout << std::endl;
-    int miles = 24;
-    std::cout << miles << " miles is " << milesToKm(miles) << " Kilometers\n\n";
-    int kms = 15;
-    std::cout << kms << " Kilometers is " << kmToMiles(kms) << " Miles\n\n";   
+    int input_val;
+    while(input_val != 3){
+        std::cout << "What conversion do you want to do?\n\t1. Miles --> Kilometers\n\t2. Kilometers --> Miles\n\t3. Exit\n";
+        std::cin >> input_val;
+        if (input_val == 1){
+            int miles;
+            std::cout << "Enter the number of miles you want to convert: ";
+            std::cin >> miles;
+            std::cout << miles << " miles is " << milesToKm(miles) << " Kilometers\n\n";    
+        }
+        if (input_val == 2){
+            int kms;
+            std::cout << "Enter the number of Kilometers you want to convert: ";
+            std::cin >> kms;
+            std::cout << kms << " Kilometers is " << kmToMiles(kms) << " Miles\n\n"; 
+        }    
+
+    }
+    
+       
 
 }
 
